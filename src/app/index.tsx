41926d8f5acd2ed729e3bcb6b9e1d768a7d8d4b4
Meferreira <mariaeduardaferreiraa7h@gmail.com>
2024-12-19 18:20:19 -0300

    montando as telas

---
+++ src/app/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text} from 'react-native';
+import { router } from 'expo-router';
+
+import { Welcome } from '@/components/welcome';
+import { Steps } from '@/components/steps';
+import { Button } from '@/components/button';
+
+
+
+export default function Index() {
+    return (
+        <View style={{
+            flex: 1, padding: 40, gap: 40
+            }}
+        >
+            <Welcome />
+            <Steps />
+            <Button onPress={() => router.navigate("/home")}>
+                <Button.Title>
+                    Next
+                </Button.Title>
+                
+            </Button>
+        </View>
+    )
+}
